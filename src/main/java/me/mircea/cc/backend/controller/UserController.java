51914d57d97d5f9e21e2841a0d1d6f29f4a9b2ff
mcdobr <mircea.dobreanu@gmail.com>
2020-05-30 15:28:34 +0300

    Add bad starter CORS config

--- src/main/java/me/mircea/cc/backend/controller/UserController.java
+++ src/main/java/me/mircea/cc/backend/controller/UserController.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import me.mircea.cc.backend.model.User;
 import me.mircea.cc.backend.service.UserService;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -17,9 +19,9 @@ import java.time.Duration;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping(value = "/hello")
-    public String hello() {
-        return "Hello there!";
+    @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> hello() {
+        return ResponseEntity.ok(Map.of("message", "Hello there!"));
     }
 
     @GetMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
