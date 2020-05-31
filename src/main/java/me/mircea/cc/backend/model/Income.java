52d31782d6b774aded357c298a1945c81ba6b541
mcdobr <mircea.dobreanu@gmail.com>
2020-05-31 21:07:33 +0300

    Add new domain models

---
+++ src/main/java/me/mircea/cc/backend/model/Income.java
@@ -0,0 +1,25 @@
+package me.mircea.cc.backend.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+@Document
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Income {
+    @Id
+    @ToString.Include
+    private String id;
+
+    private BigDecimal sum;
+}
