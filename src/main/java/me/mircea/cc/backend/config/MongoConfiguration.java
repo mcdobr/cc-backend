e2d14234a26662f17b5af2ca82aeff524009ec88
mcdobr <mircea.dobreanu@gmail.com>
2020-06-01 14:38:28 +0300

    Create config class for mongo auditing

---
+++ src/main/java/me/mircea/cc/backend/config/MongoConfiguration.java
@@ -0,0 +1,9 @@
+package me.mircea.cc.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfiguration {
+}
