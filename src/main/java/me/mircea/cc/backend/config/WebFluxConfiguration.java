cc1fe35042c7cc3052b157477b13d11107c3ca39
mcdobr <mircea.dobreanu@gmail.com>
2021-03-06 14:00:31 +0200

    Remove hack to use Google's Oauth2 implementation

--- src/main/java/me/mircea/cc/backend/config/WebFluxConfiguration.java
+++ src/main/java/me/mircea/cc/backend/config/WebFluxConfiguration.java
@@ -10,11 +10,10 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @Configuration
 @EnableWebFlux
 public class WebFluxConfiguration implements WebFluxConfigurer {
-
-    @Bean
-    public NimbusReactiveOpaqueTokenIntrospector nimbusReactiveOpaqueTokenIntrospector(OAuth2ResourceServerProperties properties) {
-        OAuth2ResourceServerProperties.Opaquetoken opaqueToken = properties.getOpaquetoken();
-        return new NimbusReactiveOpaqueTokenIntrospector(opaqueToken.getIntrospectionUri(),
-                opaqueToken.getClientId(), opaqueToken.getClientSecret());
-    }
+//    @Bean
+//    public NimbusReactiveOpaqueTokenIntrospector nimbusReactiveOpaqueTokenIntrospector(OAuth2ResourceServerProperties properties) {
+//        OAuth2ResourceServerProperties.Opaquetoken opaqueToken = properties.getOpaquetoken();
+//        return new NimbusReactiveOpaqueTokenIntrospector(opaqueToken.getIntrospectionUri(),
+//                opaqueToken.getClientId(), opaqueToken.getClientSecret());
+//    }
 }
