e10adf2cac113a48a2b850200c5d5448473f0534
mcdobr <mircea.dobreanu@gmail.com>
2021-03-13 17:23:46 +0200

    Remove expense from model

--- src/main/java/me/mircea/cc/backend/repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package me.mircea.cc.backend.repository;
-
-import me.mircea.cc.backend.model.Expense;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
-
-public interface ExpenseRepository extends ReactiveCrudRepository<Expense, Long> {
-    Flux<Expense> findAllByEmail(String email);
-}
