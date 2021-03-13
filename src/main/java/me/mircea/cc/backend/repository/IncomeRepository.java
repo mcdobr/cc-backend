0926b06d8598e7e31d2ad0b57c5518ec593f8e15
mcdobr <mircea.dobreanu@gmail.com>
2021-03-13 20:16:46 +0200

    Remove Income as separate entity

--- src/main/java/me/mircea/cc/backend/repository/IncomeRepository.java
+++
@@ -1,9 +0,0 @@
-package me.mircea.cc.backend.repository;
-
-import me.mircea.cc.backend.model.Income;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
-
-public interface IncomeRepository extends ReactiveCrudRepository<Income, Long> {
-    Flux<Income> findAllByEmail(String email);
-}
